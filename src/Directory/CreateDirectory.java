c8004fb6719da734b21918bc90e839d64a6e5eaa
mads faber <madsfaber111@live.dk>
2018-10-09 14:17:39 +0200

    not yet important

--- src/Directory/CreateDirectory.java
+++ src/Directory/CreateDirectory.java
@@ -1,4 +1,21 @@
 package Directory;
 
+import java.io.File;
+import java.io.IOException;
+
 public class CreateDirectory {
+
+    public void CreateFolder() {
+
+        String path = "C:" + File.separator + "hello" + File.separator + "hi.txt";
+// Use relative path for Unix systems
+        File f = new File(path);
+
+        f.getParentFile().mkdirs();
+        try {
+            f.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
