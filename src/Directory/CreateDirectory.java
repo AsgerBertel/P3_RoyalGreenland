6a5edb46016d1259b072f6c7748f553c0f0b08f6
mads faber <madsfaber111@live.dk>
2018-10-09 15:32:32 +0200

    folders can be created

--- src/Directory/CreateDirectory.java
+++ src/Directory/CreateDirectory.java
@@ -2,20 +2,29 @@ package Directory;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class CreateDirectory {
 
     public void CreateFolder() {
 
-        String path = "C:" + File.separator + "hello" + File.separator + "hi.txt";
-// Use relative path for Unix systems
-        File f = new File(path);
+        String fileName = "C:\\p3_folders";
 
-        f.getParentFile().mkdirs();
-        try {
-            f.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
+        Path path = Paths.get(fileName);
+
+        if (!Files.exists(path)) {
+
+            try {
+                Files.createDirectory(path);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Directory created");
+        } else {
+
+            System.out.println("Directory already exists");
         }
     }
 }
