8b70eb09facdbdb829339a088e69e2e91bc0b858
khmikkelsen <kh.mikkelsen@hotmail.com>
2018-10-10 14:15:00 +0200

    WIP

--- src/Directory/AbstractDocFolder.java
+++ src/Directory/AbstractDocFolder.java
@@ -1,12 +1,12 @@
 package Directory;
 
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public abstract class AbstractDocFolder
 {
     protected Path path;
     protected String name;
+    protected String imageName;
 
     public AbstractDocFolder(String name, Path path) {
         this.name = name;
--- src/Directory/Folder.java
+++ src/Directory/Folder.java
@@ -1,10 +1,30 @@
 package Directory;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Folder extends AbstractDocFolder
 {
+    ArrayList<AbstractDocFolder> folderContents;
+
     public Folder(String name, Path path) {
         super(name, path);
+    }
+
+    public ArrayList<AbstractDocFolder> readContent () throws IOException
+    {
+        ArrayList<AbstractDocFolder> content = new ArrayList<>();
+        Files.walk(path)
+                .filter(Files::isDirectory)
+                .forEach(file -> content.add(new Folder(file.getFileName().toString(), file.getFileName())));
+
+        Files.walk(path)
+                .filter(Files::isRegularFile)
+                .forEach(file -> content.add(new Document(file.getFileName().toString(), file.getFileName())));
+
+        return content;
     }
 }
