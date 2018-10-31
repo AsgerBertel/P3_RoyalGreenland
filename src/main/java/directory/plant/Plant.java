991f68a4e0b121432f2002feb395df26938f17c5
Hannah <hannah97@live.dk>
2018-10-31 09:45:22 +0100

    hello

--- src/main/java/directory/plant/Plant.java
+++ src/main/java/directory/plant/Plant.java
@@ -16,6 +16,19 @@ public class Plant {
         return this.accessModifier;
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
