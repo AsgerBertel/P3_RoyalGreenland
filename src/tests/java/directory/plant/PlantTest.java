60706201c9aadb17254062f5b3b2aedaf66797dd
Hannah <hannah97@live.dk>
2018-11-01 09:11:02 +0100

    alle test filer sat ind

---
+++ src/tests/java/directory/plant/PlantTest.java
@@ -0,0 +1,7 @@
+package directory.plant;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlantTest {
+
+}
---
+++ src/tests/java/gui/file_administration/FileAdminControllerTest.java
@@ -0,0 +1,7 @@
+package gui.file_administration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileAdminControllerTest {
+
+}
