665cedeeb165963c19a402a145df90c2a2770559
mads <mk96@live.dk>
2018-11-02 14:11:24 +0100

    set up basic table layout for Log tab

--- src/main/java/gui/log/LogController.java
+++ src/main/java/gui/log/LogController.java
@@ -1,4 +1,36 @@
 package gui.log;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.List;
+
 public class LogController {
+
+    @FXML
+    private TableView<rgEvent> tableView;
+
+    @FXML
+    private TableColumn<rgEvent,String> event;
+
+    @FXML
+    private TableColumn<rgEvent,String> user;
+
+    @FXML
+    private TableColumn<rgEvent,String> time;
+
+    @FXML
+    private void initialize(){
+        event.setCellValueFactory(new PropertyValueFactory<rgEvent,String>("Event"));
+        user.setCellValueFactory(new PropertyValueFactory<rgEvent,String>("User"));
+        time.setCellValueFactory(new PropertyValueFactory<rgEvent,String>("Time"));
+
+       // tableView.getItems().setAll(parseEventList);
+    }
+
+    private void parseEventList(){
+
+    }
 }
