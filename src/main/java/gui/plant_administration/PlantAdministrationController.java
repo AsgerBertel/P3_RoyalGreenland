2b5e3e1c29f2ff73cb577b71042514ea5fab94d8
saintasger <34438959+saintasger@users.noreply.github.com>
2018-11-06 12:56:42 +0100

    Layout til PlantAdministration

--- src/main/java/gui/plant_administration/PlantAdministrationController.java
+++ src/main/java/gui/plant_administration/PlantAdministrationController.java
@@ -1,47 +1,74 @@
 package gui.plant_administration;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 
-public class PlantAdministrationController {
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    private Button btnCreatePlant;
-
-    @FXML
-    private Button sidebarBtnCreatePlant;
-
-    @FXML
-    private Button btnEditPlant;
-
-    @FXML
-    private Button btnDeletePlant;
+public class PlantAdministrationController implements Initializable{
 
     @FXML
-    private Button btnOpretFabrik;
+    private AnchorPane createPane;
+
 
     @FXML
-    private Label btnPressedText;
+    private AnchorPane editPane;
+
+    @FXML
+    private StackPane switchStackPane;
+    ObservableList<Node> panes;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+        createPane.toFront();
+        editPane.setDisable(true);
+        editPane.setVisible(false);
+    }
+
 
     @FXML
     void createPlantSidebar(ActionEvent event) {
-        
+        activateCreatePane(createPane, editPane);
     }
+
+    @FXML
+    void editPlantSidebar(ActionEvent event) {
+        activateCreatePane(editPane, createPane);
+    }
+
+    private void activateCreatePane(AnchorPane activatedPane, AnchorPane disabledPane) {
+        activatedPane.setVisible(true);
+        activatedPane.setDisable(false);
+        disabledPane.setVisible(false);
+        disabledPane.setDisable(true);
+        activatedPane.toFront();
+    }
+
 
     @FXML
     void deletePlant(ActionEvent event) {
 
     }
 
-    @FXML
-    void editPlant(ActionEvent event) {
 
-    }
 
     @FXML
     void createPlant(ActionEvent event) {
+
+    }
+    @FXML
+    void editPlant(ActionEvent event){
 
     }
 
