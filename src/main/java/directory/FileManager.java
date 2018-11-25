44fb9640a512d1881bb4f2b3156e02abcbb01c84
Magnus <magnjensen@gmail.com>
2018-11-25 19:36:20 +0100

    The initial FindFolder() call no longer includes files with names starting wtih "~" (temporary word files)

--- src/main/java/directory/FileManager.java
+++ src/main/java/directory/FileManager.java
@@ -54,7 +54,7 @@ public class FileManager {
         archiveRoot = findFiles(archiveFilesRootPath);
     }
 
-    private static Folder findFiles(Path root){
+    private static Folder findFiles(Path root) {
         if (!Files.exists(root)) {
             // todo Check if server connection failure or just non-existing file throw exception maybe
         }
@@ -70,10 +70,10 @@ public class FileManager {
 
     /* Find all children of the given root and creates a list of corresponding abstractFile instances with paths
        relative to the base path */
-    private static ArrayList<AbstractFile> loadChildren(Path rootPath, Path basePath){
+    private static ArrayList<AbstractFile> loadChildren(Path rootPath, Path basePath) {
         ArrayList<AbstractFile> children = new ArrayList<>();
 
-        try {
+        try { // todo should be rewritten for readability. Use listFiles() instead - Magnus
             // Iterate through all files within the rootFolder and add them to the files list
             Files.walk(rootPath, 1)
                     .filter(path -> !path.equals(rootPath))
@@ -85,19 +85,16 @@ public class FileManager {
                             // Recursive call to load all child elements of this folder
                             folder.getContents().addAll(loadChildren(newFilePath, basePath));
                             children.add(folder);
-                        }
-                    });
-            Files.walk(rootPath, 1)
-                    .filter(path1 -> !path1.equals(rootPath))
-                    .forEach(newFilePath -> {
-                        Path relativePath = basePath.relativize(newFilePath);
+                        } else {
+                            // exclude temporary word files
+                            String fileName = relativePath.getFileName().toString();
+                            if(fileName.startsWith("~"))
+                                return;
 
-                        if(!Files.isDirectory(newFilePath)) {
                             Document document = DocumentBuilder.getInstance().createDocument(relativePath);
                             children.add(document);
                         }
                     });
-
         } catch (IOException e) {
             e.printStackTrace(); // todo handle properly
         }
@@ -160,7 +157,7 @@ public class FileManager {
     }
 
     // Creates a folder in the root directory of main files
-    public Folder createFolder(String name){
+    public Folder createFolder(String name) {
         Folder folder = new Folder(name);
 
         createFolderFile(Settings.getServerDocumentsPath() + name);
@@ -181,7 +178,7 @@ public class FileManager {
         return folder;
     }
 
-    private void createFolderFile(String fullPath){
+    private void createFolderFile(String fullPath) {
         boolean isSuccessful = new File(fullPath).mkdirs();
 
         if (!isSuccessful) {
@@ -190,22 +187,22 @@ public class FileManager {
         }
     }
 
-    public int OverwriteFilePopUP(){
+    public int OverwriteFilePopUP() {
         Alert txtInputDia = new Alert(Alert.AlertType.CONFIRMATION);
         txtInputDia.setTitle(DMSApplication.getMessage("FileManager.PopUpOverwrite.Warning"));
         txtInputDia.setHeaderText(DMSApplication.getMessage("FileManager.PopUpOverwrite.Info"));
         ButtonType buttonTypeOverwrite = new ButtonType(DMSApplication.getMessage("FileManager.PopUpOverwrite.Overwrite"));
         ButtonType buttonTypeKeep = new ButtonType(DMSApplication.getMessage("FileManager.PopUpOverwrite.Keep"));
         ButtonType buttonTypeCancel = new ButtonType(DMSApplication.getMessage("FileManager.PopUpOverwrite.Cancel"), ButtonBar.ButtonData.CANCEL_CLOSE);
-        txtInputDia.getButtonTypes().setAll(buttonTypeOverwrite,buttonTypeKeep,buttonTypeCancel);
+        txtInputDia.getButtonTypes().setAll(buttonTypeOverwrite, buttonTypeKeep, buttonTypeCancel);
 
         Optional<ButtonType> result = txtInputDia.showAndWait();
 
         txtInputDia.showAndWait();
 
-        if (result.get() == buttonTypeOverwrite){
+        if (result.get() == buttonTypeOverwrite) {
             return 1;
-        } else if (result.get() == buttonTypeKeep){
+        } else if (result.get() == buttonTypeKeep) {
             return 0;
         }
 
@@ -221,15 +218,15 @@ public class FileManager {
             // Create parent folders if they don't exist
             new File(archivePath.getParent().toString()).mkdirs();
 
-            if(Files.exists(archivePath)){
-                if(!Files.isDirectory(originalPath)){
+            if (Files.exists(archivePath)) {
+                if (!Files.isDirectory(originalPath)) {
                     // Give the document a new name if it already exists
                     archivePath = generateUniqueFileName(archivePath);
                     file.setName(archivePath.getFileName().toString());
                     Files.move(originalPath, archivePath);
                 }
-            }else{
-                Files.move(Paths.get(Settings.getServerDocumentsPath()+ file.getOSPath()), archivePath);
+            } else {
+                Files.move(Paths.get(Settings.getServerDocumentsPath() + file.getOSPath()), archivePath);
             }
 
             insertFile(file, mainFilesRoot, archiveRoot);
@@ -245,21 +242,21 @@ public class FileManager {
     }
 
     // Append incremental number at the end of file name to
-    private static Path generateUniqueFileName(Path filePath){
+    private static Path generateUniqueFileName(Path filePath) {
         Path parent = filePath.getParent();
         String extension = getExtension(filePath);
         String fileName = filePath.getName(filePath.getNameCount() - 1).toString().replace(extension, "");
 
         Path newUniquePath = filePath;
         int appendedNumber = 0;
-        while(Files.exists(newUniquePath)){
+        while (Files.exists(newUniquePath)) {
             appendedNumber++;
 
             // If the file name already has a number appended to it then replace the current number with the next one
-            if(fileName.endsWith(")") && Character.isDigit(fileName.charAt(fileName.length() - 2))){
+            if (fileName.endsWith(")") && Character.isDigit(fileName.charAt(fileName.length() - 2))) {
                 String currentNumberString = fileName.substring(fileName.lastIndexOf('('), fileName.lastIndexOf(')') + 1);
                 fileName = fileName.replace(currentNumberString, "(" + appendedNumber + ")");
-            }else{// Otherwise just append the number to the filename
+            } else {// Otherwise just append the number to the filename
                 fileName += "(" + appendedNumber + ")";
             }
             newUniquePath = parent.resolve(fileName + extension);
@@ -271,24 +268,24 @@ public class FileManager {
     /**
      * @return an empty string if the file has no extension
      */
-    private static String getExtension(Path filePath){
+    private static String getExtension(Path filePath) {
         String pathString = filePath.toString();
         int indexOfSeperator = pathString.lastIndexOf('.');
         String extension = pathString.substring(indexOfSeperator, pathString.length());
-        if(extension.contains("/") || extension.contains(File.separator))
+        if (extension.contains("/") || extension.contains(File.separator))
             return "";
 
         return extension;
     }
 
-    private String setVersionNumber(AbstractFile file){
+    private String setVersionNumber(AbstractFile file) {
         int versionNumber;
         String name1;
         String name2;
         String fileName = file.getName();
         char c = fileName.charAt(fileName.lastIndexOf(".") - 1);
 
-        if (c == ')'){
+        if (c == ')') {
             String str = fileName.substring(fileName.lastIndexOf("(") + 1,
                     fileName.lastIndexOf(")"));
 
@@ -296,7 +293,7 @@ public class FileManager {
             versionNumber++;
 
             name1 = fileName.substring(
-                    0,fileName.lastIndexOf("("));
+                    0, fileName.lastIndexOf("("));
 
             name2 = fileName.substring(
                     fileName.lastIndexOf(")") + 1,
@@ -314,7 +311,7 @@ public class FileManager {
 
         String newFileName = name1 + "(" + versionNumber + ")" + name2;
 
-        while(Files.exists(Paths.get(Settings.getServerDocumentsPath() + File.separator + newFileName))){
+        while (Files.exists(Paths.get(Settings.getServerDocumentsPath() + File.separator + newFileName))) {
             versionNumber++;
             newFileName = name1 + "(" + versionNumber + ")" + name2;
         }
@@ -328,11 +325,11 @@ public class FileManager {
         Path newPath = Paths.get(Settings.getServerDocumentsPath() + file.getOSPath().toString());
         Path oldPath = Paths.get(Settings.getServerArchivePath() + file.getOSPath().toString());
 
-        if(!Files.exists(newPath))
+        if (!Files.exists(newPath))
             Files.move(oldPath, newPath);
-        else{
+        else {
             // Find new name if it's a document and it already exists
-            if(!Files.isDirectory(oldPath)){
+            if (!Files.isDirectory(oldPath)) {
                 newPath = generateUniqueFileName(newPath);
                 Files.move(oldPath, newPath);
                 file.setName(newPath.getFileName().toString());
@@ -348,22 +345,24 @@ public class FileManager {
 
         AppFilesManager.save(this);
     }
+
     private void insertFile(AbstractFile file, Folder srcRoot, Folder dstRoot) {
-        if(file instanceof Document) {
-            insertDocument((Document)file, srcRoot, dstRoot);
-        }
-        else if (file instanceof Folder) {
+        if (file instanceof Document) {
+            insertDocument((Document) file, srcRoot, dstRoot);
+        } else if (file instanceof Folder) {
             insertFolder((Folder) file, srcRoot, dstRoot);
         }
     }
+
     private void insertFolder(Folder folder, Folder srcRoot, Folder dstRoot) {
-        for(AbstractFile file : folder.getContents()) {
-            if(file instanceof Document)
-                insertDocument((Document)file, srcRoot, dstRoot);
+        for (AbstractFile file : folder.getContents()) {
+            if (file instanceof Document)
+                insertDocument((Document) file, srcRoot, dstRoot);
             else if (file instanceof Folder)
                 insertFolder((Folder) file, srcRoot, dstRoot);
         }
     }
+
     private void insertDocument(Document document, Folder src, Folder dst) {
         Stack<Folder> stack = new Stack<>();
         Optional<Folder> parent = findParent(document, src);
@@ -371,14 +370,14 @@ public class FileManager {
         Folder temp;
         boolean folderExists = false;
 
-        while(parent.isPresent()) {
+        while (parent.isPresent()) {
             stack.push(parent.get());
             parent = findParent(parent.get(), src);
         }
-        while(!stack.empty()) {
+        while (!stack.empty()) {
             temp = new Folder(stack.peek().getPath().toString());
 
-            if(folderToInsert.getPath().toString().equals(temp.getPath().toString()))
+            if (folderToInsert.getPath().toString().equals(temp.getPath().toString()))
                 folderExists = true;
             else {
                 for (AbstractFile file : folderToInsert.getContents()) {
@@ -386,12 +385,11 @@ public class FileManager {
                         folderExists = true;
                 }
             }
-            if(!folderExists) {
+            if (!folderExists) {
                 folderToInsert.getContents().add(temp);
                 folderToInsert = temp;
-            }
-            else {
-                for(AbstractFile file : folderToInsert.getContents()) {
+            } else {
+                for (AbstractFile file : folderToInsert.getContents()) {
                     if (file.getPath().toString().equals(temp.getPath().toString()))
                         folderToInsert = (Folder) file;
                 }
@@ -404,28 +402,30 @@ public class FileManager {
     }
 
     private boolean containsByPath(Folder folder, AbstractFile target) {
-        for(AbstractFile file : folder.getContents()) {
-            if(file.getPath().toString().equals(target.getPath().toString()))
+        for (AbstractFile file : folder.getContents()) {
+            if (file.getPath().toString().equals(target.getPath().toString()))
                 return true;
         }
         return false;
     }
+
     public static Optional<Folder> findParent(AbstractFile child, Folder root) {
         Optional<Folder> parent = Optional.empty();
-        String str = root.getPath().toString()+" og "+child.getParentPath();
+        String str = root.getPath().toString() + " og " + child.getParentPath();
 
-        if(root.getPath().equals(child.getParentPath()))
+        if (root.getPath().equals(child.getParentPath()))
             return Optional.of(root);
 
         for (AbstractFile current : root.getContents()) {
             if (current instanceof Folder) {
                 parent = findParent(child, (Folder) current);
-                if(parent.isPresent())
+                if (parent.isPresent())
                     break;
             }
         }
         return parent;
     }
+
     // Saves the current instance to the json file
     public void save() {
         AppFilesManager.save(this);
@@ -440,19 +440,19 @@ public class FileManager {
     }
 
     // Looks for a File with a path corresponding to the given path
-    public Optional<AbstractFile> findInMainFiles(Path fullPath){
+    public Optional<AbstractFile> findInMainFiles(Path fullPath) {
         Path basePath = Paths.get(Settings.getServerDocumentsPath());
         Path relativePath = basePath.relativize(fullPath);
         return findFile(relativePath, getMainFiles());
     }
 
-    private Optional<AbstractFile> findFile(Path fileRelativePath, ArrayList<AbstractFile> searchArea){
-        for(AbstractFile abstractFile : searchArea){
+    private Optional<AbstractFile> findFile(Path fileRelativePath, ArrayList<AbstractFile> searchArea) {
+        for (AbstractFile abstractFile : searchArea) {
             Path filePath = abstractFile.getPath();
-            if(fileRelativePath.startsWith(filePath)){
-                if(fileRelativePath.equals(filePath)){
+            if (fileRelativePath.startsWith(filePath)) {
+                if (fileRelativePath.equals(filePath)) {
                     return Optional.of(abstractFile);
-                }else if(abstractFile instanceof Folder){
+                } else if (abstractFile instanceof Folder) {
                     return findFile(fileRelativePath, ((Folder) abstractFile).getContents());
                 }
             }
@@ -460,11 +460,11 @@ public class FileManager {
         return Optional.empty();
     }
 
-    public boolean renameFile(AbstractFile file, String newName) throws InvalidNameException{
+    public boolean renameFile(AbstractFile file, String newName) throws InvalidNameException {
         Path oldPath = Paths.get(Settings.getServerDocumentsPath() + file.getOSPath());
         Path newPath = oldPath.getParent().resolve(newName);
 
-        if(Files.exists(newPath))
+        if (Files.exists(newPath))
             throw new InvalidNameException("Name is already in use");
 
         file.setName(newName);
