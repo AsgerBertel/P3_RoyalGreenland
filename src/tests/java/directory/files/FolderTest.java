942e2ff459d4d4a623ae5d9e38fdfddba50bc91d
Hannah <hannah97@live.dk>
2018-12-05 12:12:23 +0100

    folder test started

--- src/tests/java/directory/files/FolderTest.java
+++ src/tests/java/directory/files/FolderTest.java
@@ -1,5 +1,7 @@
 package directory.files;
 
+import org.junit.jupiter.api.Test;
+
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -8,25 +10,34 @@ class FolderTest {
     private File resourcesDirectory = new File("src/tests/resTest" + File.separator);
     private Path pathToJsonTest = Paths.get(resourcesDirectory.getAbsolutePath() + File.separator + "Main Files Test/RLFiles/Server/App Files/allFiles.JSON");
 
-/*    @Test
-    void createPlant() {
-        Path pathToTestDir = Paths.get("src/tests/resTest/Main Files Test");
-        File newDirectory = new File(pathToTestDir.toString());
 
-        if (!newDirectory.exists())
-            newDirectory.mkdirs();
+    @Test
+    public void setPath(){
 
-        FileManager.getTestInstance().setPathToJson(pathToJsonTest.toString());
-
-        Folder folder = FileManager.getTestInstance().deletePlant(newDirectory.toPath(), "renameTestFolder");
-
-        folder.createPlant("renamedTestFolder");
-        assertEquals("renamedTestFolder", folder.getName());
-        assertTrue(new File(pathToTestDir.toString() + File.separator + "renamedTestFolder").exists());
-        assertTrue(new File(pathToTestDir.toString() + File.separator + "renamedTestFolder").delete());
     }
 
     @Test
-    void getContents() {
-    }*/
+    public void setName(){
+
+    }
+
+    @Test
+    public void updateChildrenPaths(){
+
+    }
+
+    @Test
+    public void getContents(){
+
+    }
+
+    @Test
+    public void containsFromAccessModifier(){
+
+    }
+
+    @Test
+    public void isSubFolderOf(){
+
+    }
 }
