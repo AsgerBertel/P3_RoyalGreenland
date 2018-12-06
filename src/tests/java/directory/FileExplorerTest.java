8b63b028430531732dca619e7aecd88656788b82
Hannah <hannah97@live.dk>
2018-12-06 10:59:53 +0100

    FileExplorer tests. couldnt complete, docs always get ID = -1 in tests.

--- src/tests/java/directory/FileExplorerTest.java
+++ src/tests/java/directory/FileExplorerTest.java
@@ -1,17 +1,60 @@
 package directory;
 
-import javafx.scene.Node;
-import javafx.scene.layout.Pane;
+import app.ApplicationMode;
+import directory.files.AbstractFile;
+import directory.files.Document;
+import directory.files.DocumentBuilder;
+import directory.plant.AccessModifier;
+import directory.plant.Plant;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FileExplorerTest {
 
+    FileExplorer fe;
+    Document docInAM;
+    Document docFalse;
+    Path pathToDoc1 = Paths.get("04_MASKINTØRRET FISK/FL 04 GR_02   Flowdiagram for produktion af maskintørret fisk.pdf");
+    Path pathToDoc2 = Paths.get("04_MASKINTØRRET FISK/PB 04 GR_02   Procesbeskrivelse for produktion af maskintørret fisk.pdf");
+    Plant plant;
+    AccessModifier am;
+
+    @BeforeEach
+    void setSettings(){
+        Settings.loadSettings(ApplicationMode.ADMIN);
+        docInAM = DocumentBuilder.getInstance().createDocument(pathToDoc1);
+        docFalse = DocumentBuilder.getInstance().createDocument(pathToDoc2);
+        ArrayList<AbstractFile> al = new ArrayList<>();
+        al.add(docInAM);
+        al.add(docFalse);
+        am = new AccessModifier();
+        am.addDocument(docInAM.getID());
+        plant = new Plant(1234, "plant", am);
+        fe = new FileExplorer(al, plant);
+    }
+
     @Test
     void getShownFiles(){
+
+        //todo documents always get ID = -1 in tests.
+
+        List<AbstractFile> shownFiles = fe.getShownFiles();
+
+        assertTrue(shownFiles.contains(docInAM));
+
+        System.out.println(shownFiles.get(0).toString() + " and " + shownFiles.get(1).toString());
+
+        System.out.println(docFalse.getID());
+
+        assertEquals(1, shownFiles.size());
 
     }
 
