8eeb4af926a79c475f9e5f22e0a62fd71ac3e315
Hannah <hannah97@live.dk>
2018-12-12 15:20:47 +0100

    stuff

--- src/tests/java/directory/FileExplorerTest.java
+++ src/tests/java/directory/FileExplorerTest.java
@@ -32,7 +32,7 @@ class FileExplorerTest extends FileTester {
     AccessModifier am;
 
     @BeforeEach
-    void setSettings(){
+    protected void setSettings(){
         SettingsManager.loadSettings(ApplicationMode.ADMIN);
         docInAM = DocumentBuilder.getInstance().createDocument(pathToDoc1);
         docFalse = DocumentBuilder.getInstance().createDocument(pathToDoc2);
