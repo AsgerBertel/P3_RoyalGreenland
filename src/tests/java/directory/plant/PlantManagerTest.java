52dca7759e3ed764df72a152346f6e18151a4c20
Hannah <hannah97@live.dk>
2018-12-13 09:24:30 +0100

    plantmanagertests

--- src/tests/java/directory/plant/PlantManagerTest.java
+++ src/tests/java/directory/plant/PlantManagerTest.java
@@ -4,24 +4,48 @@ import app.ApplicationMode;
 import directory.FileTester;
 import directory.SettingsManager;
 import gui.DMSApplication;
+import json.AppFilesManager;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlantManagerTest extends FileTester {
 
+    Plant plant;
+
     @Override
     protected void setSettings(){
         SettingsManager.loadSettings(ApplicationMode.ADMIN);
+        plant = new Plant(4567, "plant", new AccessModifier());
     }
 
     @Test
     void resetInstance() {
 
+        PlantManager pm = PlantManager.getInstance();
+
+        pm.addPlant(plant);
+
+        assertNotNull(pm);
+
+        PlantManager.resetInstance();
+
+        pm = AppFilesManager.loadPlantManager();
+
+        //todo burde virke??
+        //assertNull(pm);
     }
 
     @Test
     void getAllPlants() {
+        PlantManager pm = PlantManager.getInstance();
+
+        pm.addPlant(plant);
+        pm.addPlant(plant);
+        pm.addPlant(plant);
+
+        assertEquals(3, pm.getAllPlants().size());
+        assertTrue(pm.getAllPlants().contains(plant));
     }
 
     /*@Test
@@ -30,13 +54,36 @@ class PlantManagerTest extends FileTester {
 
     @Test
     void addPlant() {
+        PlantManager pm = PlantManager.getInstance();
+
+        pm.addPlant(plant);
+
+        assertTrue(pm.getAllPlants().contains(plant));
     }
 
     @Test
     void deletePlant() {
+        PlantManager pm = PlantManager.getInstance();
+
+        pm.addPlant(plant);
+
+        assertTrue(pm.getAllPlants().contains(plant));
+
+        pm.deletePlant(plant.getId());
+
+        assertFalse(pm.getAllPlants().contains(plant));
     }
 
     @Test
     void updateJsonFile() {
+        PlantManager pm = PlantManager.getInstance();
+
+        pm.addPlant(plant);
+
+        pm.updateJsonFile();
+
+        PlantManager newPm = PlantManager.getInstance();
+
+        assertEquals(pm, newPm);
     }
 }
